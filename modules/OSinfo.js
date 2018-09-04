4b540f88f920dfdcf47a6809c9a4688bdb908ab8
endriu17 <aolaszewski@gmail.com>
2018-09-04 12:35:25 +0200

    fix module export

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,5 +1,5 @@
 var os = require('os');
-var time = require('./time');
+var times = require('./time');
 
 function getOSinfo() {
     var type = os.type();
@@ -11,11 +11,11 @@ function getOSinfo() {
     var release = os.release();
     var cpu = os.cpus()[0].model;
     var uptime = os.uptime();
-    time.getUPTime(uptime);
     var userInfo = os.userInfo();
     console.log('System:', type);
     console.log('Release:', release);
     console.log('CPU model:', cpu);
+    console.log(times(uptime));
     console.log('User name:', userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
--- modules/time.js
+++ modules/time.js
@@ -1,8 +1,8 @@
 var os = require('os');
 
-function getUPTime(times) {
+module.exports = function (times) {
 
-    var times = os.uptime(); // to zostawiłem żebyś widział że działa
+    // var times = os.uptime(); // to zostawiłem żebyś widział że działa
     var timeH = Math.floor(times / 3600);
     var timeMin = Math.floor((times - (timeH * 3600)) / 60);
     var timeSek = Math.floor(times - (timeH * 3600) - (timeMin * 60));
@@ -13,6 +13,3 @@ function getUPTime(times) {
     }
 
 }
-
-// getUPTime();
-module.exports = getUPTime;
